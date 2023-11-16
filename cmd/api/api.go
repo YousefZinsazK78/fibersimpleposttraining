434ec529dec57e3437bd45b08ecdae0cdff7800d
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-11-16 12:12:58 +0330

    add flag and fiber.New() into api.go

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("this is just a test from yousef")
+	///set port in flag of program
+	port := flag.String("default port", ":8000", "you can set your custom font")
+	flag.Parse()
 
+	//create new instance of fiber
+	app := fiber.New()
+
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"result": "hello world"})
+	})
+
+	//listen to fiber app
+	app.Listen(*port)
 }
