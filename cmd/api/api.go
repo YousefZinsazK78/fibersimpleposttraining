41c1140c1881e7d33e78f3f8f1b572b838002fae
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-11-18 15:55:05 +0330

    add sql.Open and database information (data source name) in api.go

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/yousefzinsazk78/fiber_post_second_version/internal/routes"
+)
+
+const (
+	host     = "localhost"
+	portt    = 5432
+	user     = "postgres"
+	password = "13781378"
+	dbname   = "simpletestpostdb"
 )
 
 func main() {
@@ -11,8 +22,11 @@ func main() {
 	port := flag.String("default port", ":8000", "you can set your custom font")
 	flag.Parse()
 
-	//todo : implement database
-	//todo :
+	pgqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, portt, user, password, dbname)
+	db, err := sql.Open("postgres", pgqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	routes.Run(*port, nil)
+	routes.Run(*port, db)
 }
