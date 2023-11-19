180e48c1d74dfe8c9b13de915fa531401efbdbf1
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-11-19 17:13:03 +0330

    edit create_post_table migration file -> delete , from last item , add connection string in api.go

--- internal/database/migration/000001_create_post_table.up.sql
+++ internal/database/migration/000001_create_post_table.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS post_tbl(
      Title varchar(250) not null,
      Content varchar(300) unique not null,
      CreatedAt timestamp with time zone default CURRENT_TIMESTAMP,
-     UpdatedAt timestamp with time zone default CURRENT_TIMESTAMP,
+     UpdatedAt timestamp with time zone default CURRENT_TIMESTAMP
 );
